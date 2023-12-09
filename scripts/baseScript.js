08a01f8f3f31519e321d6a88e1c747a251413a7f
Андрей Глазков <glazkoff.andrej2014@yandex.ru>
2023-12-09 13:36:40 +0300

    Добавлен скрипт загрузки страницы

---
+++ scripts/baseScript.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", () => {
+    var loadTime = window.performance.getEntriesByType('navigation')[0].loadEventStart;
+    var textNode = document.createElement("p");
+    textNode.style.color = "white";
+    textNode.style.display = "block";
+    textNode.appendChild(document.createTextNode(loadTime + "мс - Время загрузки страницы"));
+    document.getElementsByClassName("site-footer")[0]
+    .appendChild(textNode)
+});
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     var loadTime = window.performance.getEntriesByType('navigation')[0].domContentLoadedEventStart;
+//     document.getElementsByClassName("site-footer")[0]
+//     .appendChild(document.createTextNode(loadTime))
+// })
